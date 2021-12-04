8e71076941c68407a054cc1ff2d41ddc0ae411f9
Neimhin <nrobinso@tcd.ie>
2021-12-04 17:05:55 +0000

    tfidf document order

--- tfidf.py
+++ tfidf.py
@@ -1,15 +1,15 @@
 import os
 import pickle
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from datetime import datetime as dt
 from datetime import timedelta
 
-# filenames = list(map(lambda x: 'r/coronavirus/' + x, os.listdir('r/coronavirus/')))
 def name(datetime):
-    return f'r/coronavirus_body/{datetime.date()}.csv'
+    return f'r/cleaned/text_data_{datetime.date()}.csv'
 
 def filenames_earliest_to_latest():
-    d = dt(year=2020, month=6, day=14)
+    d = dt(year=2020, month=1, day=1)
     end_date = dt(year=2021, month=12, day=31)
     while d < end_date:
         n = name(d)
@@ -17,8 +17,19 @@ def filenames_earliest_to_latest():
             yield n
         d += timedelta(days=1)
 
+
+def documents_earliest_to_latest():
+    document_order = []
+    for filename in filenames_earliest_to_latest():
+        document_order.append(filename)
+        df = pd.read_csv(filename)
+        yield df['text'][0]
+    pd.DataFrame(document_order).to_csv('document_order.csv')
+
+number_of_documents = len(list(os.listdir('r/cleaned/')))
+
 def build():
-    vectorizer = TfidfVectorizer(input='filename', ngram_range=(1,2), max_df = 0.8, min_df=5,stop_words='english')
+    vectorizer = TfidfVectorizer(input='filename', ngram_range=(1,2), max_df = 0.75, min_df=30,stop_words='english')
     X = vectorizer.fit_transform(filenames_earliest_to_latest())
     pickle.dump(vectorizer, open('tfidf_all.pickle','wb'))
     pickle.dump(X, open('tf_idf_weighted_document_term_matrix.pickle','wb'))
